b52330fe4321b6f103895709ea287c16473a94b9
nimaborokh <nimaborokh@gmail.com>
2023-10-15 21:54:02 +0330

    chang img set

--- mine.js
+++ mine.js
@@ -1,13 +1,18 @@
 let arr = [];
 function dooz(a) {
-    arr.push(1);
-    if ((arr.length + 1) % 2 === 0) {
-        return (document.getElementById(
-            a
-        ).innerHTML = `<img src="img/Screenshot 2023-10-14 184714.png"></img>`);
-    } else {
-        return (document.getElementById(
-            a
-        ).innerHTML = `<img src="img/Screenshot 2023-10-14 184803.png"></img>`);
+    if (document.getElementById(a).innerHTML == "") {
+        arr.push(1);
+        return name1(a);
+    }
+    function name1(a) {
+        if ((arr.length + 1) % 2 === 0) {
+            return (document.getElementById(
+                a
+            ).innerHTML = `<img src="img/Screenshot 2023-10-14 184714.png"></img>`);
+        } else {
+            return (document.getElementById(
+                a
+            ).innerHTML = `<img src="img/Screenshot 2023-10-14 184803.png"></img>`);
+        }
     }
 }
