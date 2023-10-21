348fbce3d334a7459aa375a8ca33b5f370d7d070
nimaborokh <nimaborokh@gmail.com>
2023-10-21 23:16:59 +0330

    add eqale

--- mine.js
+++ mine.js
@@ -2,6 +2,7 @@ let p = document.getElementById("demo");
 let btnRef = document.querySelectorAll(".btn");
 let dooze = document.getElementById("dooz");
 let arr = [];
+let booly = false;
 let winningPattern = [
     [0, 1, 2],
     [0, 3, 6],
@@ -38,7 +39,8 @@ function patternn() {
                     ((dooze.style.display = "none"),
                     (p.innerHTML = element1 + "       is  win"),
                     (document.getElementById("refresh").style.display = "")),
-                    (document.getElementById("again").style.display = "none")
+                    (document.getElementById("again").style.display = "none"),
+                    (booly = true)
                 );
             }
         }
@@ -50,111 +52,13 @@ function again() {
 function relode() {
     location.reload();
 }
-// }
-// function patternn() {
-//     let a = document.getElementById("a").innerHTML;
-//     let b = document.getElementById("b").innerHTML;
-//     let c = document.getElementById("c").innerHTML;
-//     let d = document.getElementById("d").innerHTML;
-//     let e = document.getElementById("e").innerHTML;
-//     let f = document.getElementById("f").innerHTML;
-//     let g = document.getElementById("g").innerHTML;
-//     let h = document.getElementById("h").innerHTML;
-//     let i = document.getElementById("i").innerHTML;
-//     if (a == img1 && b == img1 && c == img1) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img1 + "       is  win")
-//         );
-//     }
-//     if (d == img1 && e == img1 && f == img1) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img1 + "       is  win")
-//         );
-//     }
-//     if (g == img1 && h == img1 && i == img1) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img1 + "       is  win")
-//         );
-//     }
-//     if (a == img1 && d == img1 && g == img1) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img1 + "       is  win")
-//         );
-//     }
-//     if (b == img1 && e == img1 && h == img1) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img1 + "       is  win")
-//         );
-//     }
-//     if (c == img1 && f == img1 && i == img1) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img1 + "       is  win")
-//         );
-//     }
-//     if (a == img1 && e == img1 && i == img1) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img1 + "       is  win")
-//         );
-//     }
-//     if (c == img1 && e == img1 && g == img1) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img1 + "       is  win")
-//         );
-//     }
-//     if (a == img2 && b == img2 && c == img2) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img2 + "       is  win")
-//         );
-//     }
-//     if (d == img2 && e == img2 && f == img2) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img2 + "       is  win")
-//         );
-//     }
-//     if (g == img2 && h == img2 && i == img2) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img2 + "       is  win")
-//         );
-//     }
-//     if (a == img2 && d == img2 && g == img2) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img2 + "       is  win")
-//         );
-//     }
-//     if (b == img2 && e == img2 && h == img2) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img2 + "       is  win")
-//         );
-//     }
-//     if (c == img2 && f == img2 && i == img2) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img2 + "       is  win")
-//         );
-//     }
-//     if (a == img2 && e == img2 && i == img2) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img2 + "       is  win")
-//         );
-//     }
-//     if (c == img2 && e == img2 && g == img2) {
-//         return (
-//             (dooze.style.visibility = "hidden"),
-//             (p.innerHTML = img2 + "       is  win")
-//         );
-//     }
-// }
+function eqale() {
+    if (arr.length == 9 && booly == false) {
+        return (
+            ((dooze.style.display = "none"),
+            (p.innerHTML = "You are tied!"),
+            (document.getElementById("refresh").style.display = "")),
+            (document.getElementById("again").style.display = "none")
+        );
+    }
+}
